8a3cf6d5fcd922ff88f1ca393597ed6c6c1b5b20
maobushi <85619868+maobushi@users.noreply.github.com>
2023-01-23 07:48:04 +0900

    eleven

--- src/op/reverse_rotate.c
+++ src/op/reverse_rotate.c
@@ -1,23 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   reverse_rotate.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maobushi <maobushi@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/15 01:14:44 by maobushi          #+#    #+#             */
-/*   Updated: 2023/01/23 07:11:48 by maobushi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../push_swap.h"
 
 static void	reverse_rotate(t_stack **lst)
 {
+printf("---%d---\n", __LINE__);fflush(stdout);
+
 	t_stack	*tmp;
 
 	if (!lst || !*lst || !((*lst)->next))
 		return ;
+printf("---%d---\n", __LINE__);fflush(stdout);
 	tmp = ft_lstlast(*lst);
 	tmp->next = *lst;
 	(*lst)->prev = tmp;
@@ -25,6 +16,7 @@ static void	reverse_rotate(t_stack **lst)
 	tmp = tmp->prev;
 	tmp->next = NULL;
 	(*lst)->prev = NULL;
+printf("---%d---\n", __LINE__);fflush(stdout);
 }
 
 void	rra(t_stack **input)
